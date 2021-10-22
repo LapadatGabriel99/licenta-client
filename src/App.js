6db3f3b1f806c242fae80c2df6bc8e19269ae07e
Gabi Lapadat <lapadatrobert123@gmail.com>
2021-10-23 02:11:14 +0300

    Client first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Form, FormGroup } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form>
+        <FormGroup>
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text"
+                        placeholder='Enter username'
+                        name='username'/>
+        </FormGroup>
+        <FormGroup>
+          <Form.Label></Form.Label>
+        </FormGroup>
+      </Form>
     </div>
   );
 }

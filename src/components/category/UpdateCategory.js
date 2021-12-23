1560983ec1dbfe295c4603aafc88ca342c0f1d09
Gabi Lapadat <lapadatrobert123@gmail.com>
2021-12-23 15:34:52 +0200

    Update category and update test pages.

--- src/components/category/UpdateCategory.js
+++ src/components/category/UpdateCategory.js
@@ -1,10 +1,116 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button, Col, Container, Form, Row } from 'react-bootstrap'
+import { shallowEqual } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { getCategory, updateCategory } from '../../actions/category/categoryActions'
+import useForm from '../../hooks/useForm'
+import '../../styles/Create.css'
+
+const initialFormValues = {
+    id: '',
+    testType: ''
+}
 
 function UpdateCategory(props) {
+
+    const { categoryId } = props
+
+    const dispatch = useDispatch()
+
+    const category = useSelector(state => state.category.category)
+
+    const history = useHistory()
+
+    const [reload, setReload] = useState(false)
+
+    useEffect(() => {
+
+        const fetchCategory = async (id) => {
+
+            await dispatch(getCategory(id, history))
+
+            setReload(prev => !prev)
+        }
+
+        fetchCategory(categoryId)
+    }, [])
+
+    useEffect(() => {
+
+        setValues(prev => {
+
+            return {
+                id: category.id,
+                testType: category.testType
+            }
+        })
+    }, [reload])
+
+    const validate = (fieldValues = values) => {
+        let temp = {...errors}
+
+        if ('testType' in fieldValues) {
+            temp.testType = fieldValues.testType ? '' : 'This field is required'
+        }
+
+        setErrors({
+            ...temp
+        })
+
+        if (fieldValues == values)
+            return Object.values(temp).every(x => x == "")
+    }
+
+    const [ values, 
+        setValues, 
+        errors, 
+        setErrors, 
+        handleInputChange, 
+        resetForm ] = useForm(initialFormValues, true, validate)
+
+    const onSubmitHandler = (e) => {
+
+        e.preventDefault()
+
+        const categoryDTO = {
+            id: category.id,
+            testType: category.testType
+        }
+
+        if (category.testType === "") {
+
+            return (window.confirm("Category name cannot be empty"))
+        }
+
+        updateCategory(categoryDTO, history)
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Container className="text-center">
+            <Row>
+                <Col className="col-md-8 m-auto">
+                    <h3 className="font-weight-normal">Update category</h3>
+                    <Form className="create-form" onSubmit={onSubmitHandler}>
+                        <Form.Group className="form-group-spacing">
+                            <Form.Label>Category Name</Form.Label>
+                            <Form.Control   type="text"
+                                            name="testType"
+                                            value={category.testType}
+                                            placeholder="Category name"
+                                            onChange={handleInputChange}
+                                            error={errors.testType}/>
+                        </Form.Group>
+                        <div className="d-grid gap-2 mt-4">
+                            <Button className="btn-lg btn-dark btn-block"
+                                type='submit'>
+                                Update Category
+                            </Button>
+                        </div>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
--- src/components/test/UpdateTest.js
+++ src/components/test/UpdateTest.js
@@ -1,10 +1,174 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button, Col, Container, Form, Row } from 'react-bootstrap'
+import { shallowEqual } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { getCategories, getCategory, updateCategory } from '../../actions/category/categoryActions'
+import { getTest, updateTest } from '../../actions/test/testActions'
+import useForm from '../../hooks/useForm'
+import '../../styles/Create.css'
 
-function UpdateTest() {
+const initialFormValues = {
+    'id': '',
+    'name': ''
+}
+
+function UpdateTest(props) {
+
+    const { testId } = props
+
+    const dispatch = useDispatch()
+
+    const categories = useSelector(state => state.category.categories)
+
+    const test = useSelector(state => state.test.test)
+
+    const history = useHistory()
+
+    const [reload, setReload] = useState(false)
+
+    useEffect(() => {
+
+        const fetchData = async (id) => {
+
+            await dispatch(getTest(id, history))
+
+            await dispatch(getCategories(history))
+        
+            setReload(prev => !prev)
+        }
+
+        fetchData(testId)
+    }, [])
+
+    useEffect(() => {
+
+        setValues(prev => {
+
+            return {
+                id: test.id,
+                name: test.name
+            }
+        })
+    }, [reload])
+
+    const validate = (fieldValues = values) => {
+        let temp = {...errors}
+
+        if ('name' in fieldValues) {
+            temp.name = fieldValues.name ? '' : 'This field is required'
+        }
+
+        setErrors({
+            ...temp
+        })
+
+        if (fieldValues == values)
+            return Object.values(temp).every(x => x == "")
+    }
+
+    const [ values, 
+        setValues, 
+        errors, 
+        setErrors, 
+        handleInputChange, 
+        resetForm ] = useForm(initialFormValues, true, validate)
+
+    const onSelectListChangedHandler = (e) => {
+
+        if(e.target.value == null || e.targe.value === "") {
+            window.alert("Please select a valid category!")
+            return
+        }
+
+        const category = categories.filter(c => c.testType === e.target.value)
+        setValues(prev => {
+            return {
+                ...prev,
+                categoryId: category[0].id,
+                categoryName: e.target.value
+            }
+        })
+    }
+
+    const onSubmitHandler = (e) => {
+
+        e.preventDefault()
+
+        const put = async () => {
+
+            await dispatch(updateTest({
+                'name': values.name,
+                'categoryId': values.categoryId,
+                'categoryName': values.categoryName
+            }, history))
+        }
+
+        if (validate()) {
+
+            put()
+        }
+    }
+
+    const categoryList = () => {
+
+        if (categories != null) {
+            let items = [];
+
+            items.push(<option key="" value="">Select category</option>)
+
+            for (let i = 0; i < categories.length; i++) {
+
+                const element = categories[i]
+                items.push(<option key={element.id} value={element.testType}>
+                                {element.testType}
+                            </option>)
+            }
+
+            return(
+                <select className="form-control form-control-lg"
+                        name="category"
+                        value={values.categoryName}
+                        onChange={onSelectListChangedHandler}>
+                        {
+                            items
+                        }
+                </select>
+            )
+        }
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Container className="text-center">
+            <Row>
+                <Col className="m-auto">
+                    <h3 className="font-weight-normal">Update Test</h3>
+                    <Form className="create-form" onSubmit={onSubmitHandler}>
+                        <Form.Group className="form-group-spacing">
+                            <Form.Label>Test Name</Form.Label>
+                            <Form.Control type="text"
+                                          placeHolder="Test name"
+                                          name="name"
+                                          value={values.name}
+                                          onChange={handleInputChange}
+                                          error={errors.name}/>
+                        </Form.Group>
+                        <Form.Group>
+                            {
+                                categoryList()
+                            }
+                        </Form.Group>
+                        <div className="d-grid gap-2 mt-4">
+                            <Button className="btn-lg btn-dark"
+                                type="submit"
+                                > 
+                                Update
+                            </Button>
+                        </div>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 

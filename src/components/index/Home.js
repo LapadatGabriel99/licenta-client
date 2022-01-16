3cd7ee6ec00b4a8035bbd9e28e4ac750249022a2
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-01-16 15:55:32 +0200

    Refactor.

--- src/components/index/Home.js
+++ src/components/index/Home.js
@@ -1,11 +1,51 @@
 import React from 'react'
+import { Carousel, Container } from 'react-bootstrap'
 import '../../styles/Home.css'
+import firstSlide from '../../resources/first-slide.svg';
+import secondSlide from '../../resources/second-slide.svg';
+import thirdSlide from '../../resources/third-slide.svg';
 
 function Home(props) {
     return (
-         <h1 className="main-text-area">
-            Test User Tool
-        </h1>
+        <Container className="main-text-area">
+            <Carousel className="main-carousel-area drop-shadow">
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={firstSlide}
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={secondSlide}
+                    alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={thirdSlide}
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </Container>
     )
 }
 
--- src/components/user/UserHub.js
+++ src/components/user/UserHub.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import '../../styles/UserHub.css'
 
 function UserHub() {
     return (
-        <h1>
-           Hello World! 
-        </h1>
+        <div className="container bg-dark text-light p-5 main-text-area drop-shadow">
+            <div className="container bg-dark p-5">
+                <h1 className="display-4">Welcome to demo app</h1>
+                <p className="lead">This is the user-hub of our application. You can access it only if you have signed in.</p>
+                <hr className="my-4"/>
+                <p className="h4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p className="lead">
+                    <a href="#" className="btn btn-success btn-lg">Lorem Ipsum is simply dummy text</a> 
+                </p>
+            </div>
+        </div>
     )
 }
 
